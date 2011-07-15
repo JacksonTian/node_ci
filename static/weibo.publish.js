e39885ad8a74b6341d61d53b4c15094f0ce6352f
Jackson Tian <shyvo1987@gmail.com>
2011-07-15 02:52:07 -0700

    Remove the useless files.

--- static/weibo.publish.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var publishButton = $("#tweet_box button");
-    publishButton.click(function () {
-        $.post("/sina/publishTweet", {"status": $("#tweet_box textarea").val()}, function (data) {
-            console.log(data);
-        });
-    });
-});
